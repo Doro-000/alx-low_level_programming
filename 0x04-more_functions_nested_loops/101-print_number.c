9b1b98143234f3ce78540c8ef852bbb192e07a1a
Doro-000 <hizkyastekle@gmail.com>
2021-02-11 19:16:26 +0000

    update solution

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <math.h>
 
 /**
  * print_number - prints a number using _putchar
@@ -9,36 +8,18 @@
  */
 void print_number(int n)
 {
-	int counter = 0, tmp, i, flag = 0;
+	unsigned int num = n;
 
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar('0');
+		_putchar('-');
+		num *= -1;
 	}
-	else
+
+	if (num / 10)
 	{
-		if (n < 0)
-		{
-			n *= -1;
-			flag = 1;
-		}
-
-		tmp = n;
-
-		while (tmp)
-		{
-			tmp /= 10;
-			counter++;
-		}
-
-		if (flag)
-		{
-			_putchar('-');
-		}
-
-		for (i = counter - 1; i >= 0; i--)
-		{
-			_putchar(((n / (int) pow((double) 10, (double) i)) % 10) + '0');
-		}
+		print_number(num / 10);
 	}
+
+	_putchar(num % 10 + '0');
 }
