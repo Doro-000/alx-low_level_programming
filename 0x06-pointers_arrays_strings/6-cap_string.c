59506f976edb502d59c51e3223459b8b198a7fc8
Doro-000 <hizkyastekle@gmail.com>
2021-02-18 11:25:15 +0000

    made betty happy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,13 @@
  */
 char *cap_string(char *str)
 {
-	int i = 1;
+	int i = 0;
 
 	if (str[i] <= 'a' && str[i] >= 'z')
+	{
 		str[i] -= 32;
+		i++;
+	}
 	while (str[i] != '\0')
 	{
 		if (is_separator(str[i]) && (str[i + 1] >= 'a'
