bd63ce0460e8df75b1967dafdd66d235d6d5e762
Doro-000 <hizkyastekle@gmail.com>
2021-02-18 01:56:58 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+void swap_char(char *a, char *b);
+char *_strcat(char *dest, char *src);
