88e08a513359c7b7818e615a36193a50fe3dc5a5
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-22 01:04:37 -0800

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
 
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix 
+ * @a: array to be used
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
+void print_chessboard(int *a, int size)
+{
+	int i = 0, sum_1 = 0, sum_2 = 0;
+
+	for (; i < size; i++)
+	{
+		sum_1 += a[i][i];
+		sum_2 += a[size - i - 1][i];
+	}
+	printf("%d, %d\n", sum_1, sum_2);
+}
