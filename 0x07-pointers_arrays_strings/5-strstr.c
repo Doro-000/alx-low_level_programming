06b02772182c39700e0f422d9dc64dc86cbc67c9
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-22 03:45:04 -0800

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		{
 			while (needle[j] != '\0')
 			{
-				if (haystack[i + j] != needle[j])
+				if (haystack[i + j] != needle[j] && haystack[i + j] != '\0')
 				{
 					flag = 0;
 					break;
