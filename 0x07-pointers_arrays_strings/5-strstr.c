fffafb666e95893cc562d3863ea9b1ffb862425b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-22 05:19:16 -0800

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,36 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0, flag = 0;
+
+	if (*needle = '\0')
+	{
+		return (NULL);
+	}
+
+	for (; haystack[i] != '\0'; i++)
+	{
+		int j = 0;
+
+		if (haystack[i] == needle[0])
+		{
+			while (needle[j] != '\0')
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					flag = 0;
+					break;
+				}
+				else
+					flag = 1;
+				j++;
+			}
+			if (flag)
+				break;
+		}
+	}
+	if (flag)
+		return (haystack + i);
+	else
+		return (NULL);
 }
