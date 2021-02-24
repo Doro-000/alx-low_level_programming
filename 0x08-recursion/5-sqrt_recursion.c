a38429a644588cacac4698bc083f8de03756c4e5
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-24 06:42:32 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,16 @@
+#include "holberton.h"
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be used
+ *
+ * Return: the square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	int temp = n / 2;
+	if (n < 0)
+		return (-1);
+	else if (n == 1 || n == 0)
+		return (n);
+}
