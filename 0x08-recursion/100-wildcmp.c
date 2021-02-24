e9c30f013c26aaae51e65994ae687f1d4f509113
Doro-000 <hizkyastekle@gmail.com>
2021-02-24 19:04:04 +0000

    betty be happy

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -2,18 +2,18 @@
 
 /**
  * wildcmp - compares two strings and returns 1 if identical
- * @s1 - string to be checked
- * @s2 - pattern to be used
+ * @s1: string to be checked
+ * @s2: pattern to be used
  *
  * Return: 1 if identical, 0 otherwise
  */
 int wildcmp(char *s1, char *s2)
 {
-	if(*s2 == '\0' && *s1 == '\0')
+	if (*s2 == '\0' && *s1 == '\0')
 		return (1);
-	if(*s2 == *s1)
-		return wildcmp(s1 + 1, s2 + 1);
-	if(*s2 == '*')
-		return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
