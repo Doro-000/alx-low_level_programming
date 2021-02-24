c4f7e68ff0337404743bad1f401a9bcc29f47f41
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-24 06:48:05 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,14 @@
+#include "holberton.h"
 
+/**
+ * is_prime_number - returns the 1 if n is prime
+ * @n: number to be checked
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	
+}
