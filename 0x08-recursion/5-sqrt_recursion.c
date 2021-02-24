a3587e23b9392438d0391baeed97098f4a5b6259
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-24 13:47:51 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,10 +10,8 @@ int _sqrt_recursion(int n)
 {
 	int temp = n / 2;
 
-	if (n == 1)
-		return (1);
-	else if (n == 0)
-		return (0);
+	if (n == 1 || n == 0)
+		return (n);
 	return (_sqrt(temp, n));
 }
 
