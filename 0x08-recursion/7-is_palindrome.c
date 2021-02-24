977accd924c2b14f1f6b0d71d82c914a5d0b7967
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-24 16:32:13 +0300

    Update 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -8,24 +8,4 @@
  */
 int is_palindrome(char *s)
 {
-	int start = n / 2;
-
-	if (n <= 1)
-		return (0);
-	return(is_prime (n, start));
-}
-
-/**
- * _print_rev_recursion - prints a string in reverse
- * @s: string to be printed
- *
- * Return: void
- */
-char reverse_string(char *output, char *src)
-{ 
-	if (*src == '\0')
-	{
-		reverse_string(src + 1);
-	}
-	*src = *(src - 1));
 }
