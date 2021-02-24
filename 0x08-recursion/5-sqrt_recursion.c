cd9e027916d1b9aaefc1648f242cb88534c70a0e
Doro-000 <hizkyastekle@gmail.com>
2021-02-24 10:25:22 +0000

    betty be happy

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,7 +9,7 @@ int _sqrt_recursion(int n)
 	int temp = n / 2;
 
 	if (n == 1 || n == 0)
-	    return (n);
+		return (n);
 	return (_sqrt(temp, n));
 }
 
@@ -25,6 +25,6 @@ int _sqrt(int n, int x)
 	if (n * n == x)
 		return (n);
 	if (n <= 0)
-	    return (-1);
+		return (-1);
 	return (_sqrt(n - 1, x));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 		return (0);
-	return(is_prime (n, start));
+	return (is_prime(n, start));
 }
 
 /**
@@ -24,9 +24,9 @@ int is_prime_number(int n)
  */
 int is_prime(int n, int start)
 {
-    	if (start <= 1)
+	if (start <= 1)
 		return (1);
 	else if (n % start == 0)
 		return (0);
-	return(is_prime (n, start - 1));
+	return (is_prime(n, start - 1));
 }
