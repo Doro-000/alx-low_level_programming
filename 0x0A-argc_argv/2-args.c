1ae9bef93554e35612f0ba2a2b2b8dabd9c2c271
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-26 17:31:48 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+/**
+ * main - entry point prints all the arguments passed to it
+ *
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
