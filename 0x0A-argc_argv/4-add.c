37626b5612d641a742a2cec6ba8b9050f7c9a79a
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-26 18:14:09 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - entry point prints the sum of a variable amount of numbers
+ *
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		while (--argc > 0)
+		{
+			if (atoi(argv[argc]) != 0)
+				sum += atoi(argv[argc]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+}
