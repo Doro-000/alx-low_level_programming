eb03ea4c35e8562895519f62ee7a5f19538e2d42
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-26 17:43:28 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - entry point prints the product of 2 numbers
+ *
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", (*argv[1]) * (*argv[2]));
+		return (0);
+	}
+}
