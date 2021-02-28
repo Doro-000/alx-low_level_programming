f5ff375b1428c7007dfcd06b31d5d79734a17d59
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-01 00:00:30 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,126 +1,75 @@
-// #include "holberton.h"
-
-// /**
-//  * main - entry point, calls the get_change function
-//  * @argc: number of args passed
-//  * @argv: array of args
-//  *
-//  * Return: 0, 1 if unsuccessful
-//  */
-// int main(int argc, char *argv[])
-// {
-// 	int coins[] = {1, 2, 5, 10, 25};
-// 	int change;
-
-// 	if (argc != 2)
-// 	{
-// 		printf("Error\n");
-// 		return (1);
-// 	}
-// 	change = get_change(coins, atoi(argv[1]), sizeof(coins) / sizeof(int));
-// 	printf("%d\n", change);
-// 	return (0);
-// }
-
-// /**
-//  * get_change - calculates minimum number of coins to make change for amount
-//  * @amount: amount of money change is to be calculated for
-//  * @coins: given coins
-//  * @size: amount of coins
-//  *
-//  * Return: the calculated result
-//  */
-// int get_change(int coins[], int amount, int size)
-// {
-// 	int **table;
-// 	int i, j;
-
-// 	table = (int **) malloc(sizeof(int *) * size);
-// 	for (i = 0; i < size; i++)
-// 		table[i] = (int *) malloc(sizeof(int) * amount + 1);
-// 	if (amount <= 0)
-// 	{
-// 		return (0);
-// 	}
-// 	for (i = 0; i < size; i++)
-// 	{
-// 		table[i][0] = 0;
-// 	}
-// 	for (i = 0; i < size; i++)
-// 	{
-// 		for (j = 1; j <= amount; j++)
-// 		{
-// 			if (i > 0)
-// 			{
-// 				if (coins[i] > j)
-// 				{
-// 					table[i][j] = table[i - 1][j];
-// 				}
-// 				else
-// 				{
-// 					table[i][j] = min(table[i - 1][j], 1 + table[i][j - coins[i]]);
-// 				}
-// 			}
-// 			else
-// 			{
-// 				if (coins[i] <= j)
-// 					table[i][j] = 1 + table[i][j - coins[i]];
-// 				else
-// 					table[i][j] = j;
-// 			}
-// 		}
-// 	}
-// 	free(table);
-// 	return (table[size - 1][amount]);
-// }
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * main - main function
- * @argc: number of arguments
- * @argv: array of pointers
- * Return: 0
+ * main - entry point, calls the get_change function
+ * @argc: number of args passed
+ * @argv: array of args
+ *
+ * Return: 0, 1 if unsuccessful
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int s, n;
+	int coins[] = {1, 2, 5, 10, 25};
+	int change;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	s = atoi(argv[1]);
-	while (s > 0)
+	change = get_change(coins, atoi(argv[1]), sizeof(coins) / sizeof(int));
+	printf("%d\n", change);
+	return (0);
+}
+
+/**
+ * get_change - calculates minimum number of coins to make change for amount
+ * @amount: amount of money change is to be calculated for
+ * @coins: given coins
+ * @size: amount of coins
+ *
+ * Return: the calculated result
+ */
+int get_change(int coins[], int amount, int size)
+{
+	int **table;
+	int i, j;
+
+	table = (int **) malloc(sizeof(int *) * size);
+	for (i = 0; i < size; i++)
+		table[i] = (int *) malloc(sizeof(int) * amount + 1);
+	if (amount <= 0)
 	{
-		if (s >= 25)
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		table[i][0] = 0;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j <= amount; j++)
 		{
-			n++;
-			s = s - 25;
-		}
-		else if (s >= 10)
-		{
-			n++;
-			s = s - 10;
-		}
-		else if (s >= 5)
-		{
-			n++;
-			s = s - 5;
-		}
-		else if (s >= 2)
-		{
-			n++;
-			s = s - 2;
-		}
-		else if (s >= 1)
-		{
-			n++;
-			s = s - 1;
+			if (i > 0)
+			{
+				if (coins[i] > j)
+				{
+					table[i][j] = table[i - 1][j];
+				}
+				else
+				{
+					table[i][j] = min(table[i - 1][j], 1 + table[i][j - coins[i]]);
+				}
+			}
+			else
+			{
+				if (coins[i] <= j)
+					table[i][j] = 1 + table[i][j - coins[i]];
+				else
+					table[i][j] = j;
+			}
 		}
 	}
-	printf("%d\n", n);
-	return (0);
+	free(table);
+	return (table[size - 1][amount]);
 }
