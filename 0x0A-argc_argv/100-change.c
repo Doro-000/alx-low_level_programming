b34b1c9f20b9643ca14343b23ad2df59455d472d
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-28 22:54:26 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -30,9 +30,10 @@ int main(int argc, char *argv[])
  *
  * Return: the calculated result
  */
-int get_change(int coins[], int amount, int size)
+int get_change(int coins[], int a, int size)
 {
-	int table[size][amount + 1];
+	int amount = AMOUNT(a);
+	int table[SIZE][amount + 1];
 	int i, j;
 
 	if (amount < 0)
