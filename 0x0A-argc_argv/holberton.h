0a7469a619f0026fa6f3251741e27b178abdf62f
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-02-28 23:49:29 +0300

    Update holberton.h

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -7,6 +7,6 @@
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int is_valid(char *s);
-int get_change(int coins[], int a, int size);
+int get_change(int coins[], int amount, int size);
 
 #endif /*HOLBERTON_H*/
