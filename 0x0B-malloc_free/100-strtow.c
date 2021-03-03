0241dad4e60aad89d863396d1bcf43ba1d3ba448
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-03 01:12:50 -0800

    Update 100-strtow.c

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -11,7 +11,7 @@ char **strtow(char *str)
 	char **split;
 	int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
-	if (str == NULL)
+	if (words == 0)
 		return (NULL);
 	split = (char **) malloc(sizeof(char *) * (words + 1));
 	if (split != NULL)
