5ac7aa0558d1b3bbd480272b945cdbc8fd688427
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-04 05:33:15 -0800

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,7 +10,7 @@
 int *array_range(int min, int max)
 {
 	int *block;
-	int i;
+	int i, j = 0;
 
 	if (min > max)
 		return (NULL);
@@ -18,7 +18,10 @@ int *array_range(int min, int max)
 	if (block != NULL)
 	{
 		for (i = min; i <= max; i++)
-			block[i] = i;
+		{
+			block[j] = i;
+			j++
+		}
 		return (block);
 	}
 	else
