b4ffff012008b117bdae6a31ea61adee97c75c86
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-04 06:30:52 -0800

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,17 +13,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *temp_block;
 	unsigned int i;
 
-	if (new_size == old_size)
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else if (new_size == old_size)
 		return (ptr);
 	else if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
-	}
-	else if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		return (ptr);
 	}
 	else
 	{
