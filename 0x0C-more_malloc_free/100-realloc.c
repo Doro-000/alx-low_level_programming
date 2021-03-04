1810caf9acfedc24825aa702acb5482b93a49525
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-04 05:55:34 -0800

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	else if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	else if (ptr == NULL)
