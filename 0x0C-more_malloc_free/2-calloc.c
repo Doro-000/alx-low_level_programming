77698cd28df1dfec3323eab74b1ea18d0fc255a7
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-03 23:38:29 -0800

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,26 @@
 #include "holberton.h"
 
 /**
- * malloc_checked - allocates memory using malloc, exits with status 98 if it fails
- * @b: size of the memory block to be allocated
+ * _calloc - allocates memory using malloc, and initializes it to zero
+ * @size: size of the memory block to be allocated
+ * @nmemb: number of elements
  *
  * Return: pointer to the address of the memory block
  */
-void *malloc_checked(unsigned int b)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *block;
+	int i;
 
-	block = malloc(b);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	block = malloc(nmemb * size);
 	if (block != NULL)
+	{
+		for (i = 0; i < nmemb; i++)
+			*(int *)(block + i) = 0;
 		return (block);
+	}
 	else
-		exit(98);
+		return (NULL);
 }
