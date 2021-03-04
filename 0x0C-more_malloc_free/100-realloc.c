948a29d8f107fbedbbede5d1152dfdc417d4e806
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-04 05:41:49 -0800

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			if (ptr != NULL)
 			{
 				for (i = 0; i < old_size; i++)
-					*(int *)(ptr + i) = *(int *)(temp_block + i);
+					*((int *) ptr + i) = *((int *) temp_block + i);
 				return (ptr);
 			}
 			else
