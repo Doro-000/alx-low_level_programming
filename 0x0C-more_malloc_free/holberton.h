672c2f4f58b2e91c26b2122d6c8588790b3c3cb1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-03 23:45:49 -0800

    Update holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,14 +5,10 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int num_words(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif /*HOLBERTON_H*/
