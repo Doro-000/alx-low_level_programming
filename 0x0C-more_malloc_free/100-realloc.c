6b0cf221b142b114d4ad5fc59998a086285cd2d0
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-04 00:20:35 -0800

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,17 +1,47 @@
 #include "holberton.h"
 
 /**
- * malloc_checked - allocates memory using malloc, exit(98) if it fails
- * @b: size of the memory block to be allocated
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: size of ptr
+ * @new_size: size of the new memory to be allocated
  *
- * Return: pointer to the address of the memory block
+ * Return: pointer to the address of the new memory block
  */
-void *malloc_checked(unsigned int b)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *block;
+	void *temp_block;
+	int i;
 
-	block = malloc(b);
-	if (block == NULL)
-		exit(98);
-	return (block);
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr != NULL) 
+	{
+		free(ptr);
+		return (NULL);	
+	}
+	else if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else
+	{
+		temp_block = malloc(old_size);
+		if (temp_block != NULL)
+		{
+			free(ptr);
+			ptr = malloc(new_size);
+			if (ptr != NULL)
+			{
+				for (i = 0; i < old_size; i++)
+					*(int *)(ptr + i) = *(int *)(temp_block + i);
+				return (ptr);
+			}
+			else
+				return (NULL);
+		}
+		else
+			return (NULL);
+	}
 }
