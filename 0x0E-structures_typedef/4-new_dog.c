d2f244da462a6d715b9881c477080617502d8f46
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-08 06:52:25 -0800

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  *
  * Return: pointer to the new dog_t
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *temp_name, *temp_owner;
 	dog_t new_doggo;
