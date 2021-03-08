7be99e372a5ee1e28a1e56a4285bc5e5a7ddf073
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-08 05:53:13 -0800

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/**
+ * struct dog - a structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
