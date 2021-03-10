14cab7a05a27cbca954f043f8470316fa6364f5f
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 13:54:44 +0300

    Create 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of the main function
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments passed to the function
+ *
+ * Return: 0 on success, 1 or 2 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, char *);
+	int i = 0, bytes;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	f = main;
+	bytes = atoi(argv[1]);
+	for (; i < bytes; i++)
+	{
+		printf("%2x ", *(f + 1));
+	}
+	printf("\n");
+	return (0);
+}
