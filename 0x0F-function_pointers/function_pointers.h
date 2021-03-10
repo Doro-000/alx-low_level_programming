340c204a1b0cf51ed305d511eb6bc16a3c40f048
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 12:31:08 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,12 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /*HOLBERTON_H*/
+#endif /*FUNCTION_POINTERS_H*/
