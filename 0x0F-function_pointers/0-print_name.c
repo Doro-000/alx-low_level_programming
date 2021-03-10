3edbabdb371c33acdaa14cbcf6c40ff381079731
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 09:42:36 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be used
+ * @f: callback function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
