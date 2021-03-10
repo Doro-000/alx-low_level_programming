9f52087c6c7c6d9874c4222791d1ce194e961b2d
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 12:07:59 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func -  selects the correct function to perform the operation
@@ -21,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i <= 5)
 	{
-		if (*s == ops[i].op)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
