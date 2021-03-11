5fc8594b7f10583d7abda2873e9b61e6f0318ecd
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 22:58:05 -0800

    Update 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -  adds all the parameters passed to it
+ * @n: number of args passed
+ *
+ * Return: sum of all the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+}
