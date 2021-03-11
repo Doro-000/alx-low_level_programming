7266965d0be62fbec6b6cf989cdbbedb5faa146e
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 23:31:24 -0800

    Update 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @n: number of args passed
+ * @separator: string to be printed between strings
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list params;
+
+	if (n != 0)
+	{
+		va_start(params, n);
+		for (i = 0; i < n; i++)
+		{
+			if (i != n - 1)
+			{
+				if (separator != NULL)
+					printf("%s%s", point_to_nil(va_arg(params, char *)), separator);
+				else
+					printf("%s", point_to_nil(va_arg(params, char *)));
+			}
+			else
+			{
+				printf("%s", point_to_nil(va_arg(params, char *)));
+			}
+		}
+	}
+	printf("\n");
+}
