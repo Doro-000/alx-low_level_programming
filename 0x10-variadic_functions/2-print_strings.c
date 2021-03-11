9e8016640bfb97e0dde33345d0c2aa49910469a0
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-10 23:17:54 -0800

    Update 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -  adds all the parameters passed to it
+ * @n: number of args passed
+ *
+ * Return: sum of all the parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i;
+	va_list params;
+
+	if (n != 0)
+	{
+		va_start(params, n);
+		for (i = 0; i < n; i++)
+			sum += va_arg(params, int);
+	}
+	return (sum);
+}
