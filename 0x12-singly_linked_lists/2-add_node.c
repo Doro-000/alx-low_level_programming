05e4b802ca14505850d8bd732c3ee35b6cc39753
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-18 19:58:09 +0300

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+	const char *string;
+	list_t *new_node;
+	list_t *temp;
+	
+	new_node = malloc(sizeof(list_t));
+	if ((new_node != NULL) && (*head != NULL))
+	{
+		new_node->str = stdup(str);
+		new_node->len = strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+		return (NULL);
+}
