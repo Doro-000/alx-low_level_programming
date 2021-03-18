f1fd1e6d6782e0e33e2b95e5fd1a0c7793ce8530
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-18 20:29:27 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	const list_t *cursor = h;
