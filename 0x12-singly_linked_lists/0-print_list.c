497da9d83388bbf9cc54906ee0f18d0e7f840ad2
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-18 20:27:30 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	const list_t *cursor = h;
