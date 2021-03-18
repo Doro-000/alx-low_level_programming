9ef5645d20a76e2c721b6e8032e030cc62eebd5d
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-18 19:10:43 +0300

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	const list_t *cursor = h;
+	size_t count;
+
+	while (cursor != NULL)
+	{
+		if (cursor->str != NULL)
+			printf("[%d] %s\n", cursor->len, cursor ->str);
+		else
+			printf("[0] (nil)\n");
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
