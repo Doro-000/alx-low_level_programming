b1360ab71f8f803c5f2ad1052c361f04d579edd7
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-23 14:18:16 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,49 +10,41 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL;
 	listint_t *previous_node = NULL;
-	listint_t *cursor;
-	unsigned int i = 0;
+	listint_t *new_node = NULL;
+	int i = 0;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (head != NULL)
 	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-
-	cursor = *head;
-	while (cursor != NULL)
-	{
-		if (i <= idx)
+		new_node = malloc(sizeof(listint_t));
+		if (new_node != NULL)
 		{
-			if (i == idx)
+			new_node->n = n;
+			new_node->next = NULL;
+		}
+		while (*head != NULL)
+		{
+			if ((i + 1) == idx)
+				previous_node = *head;
+			else if (i <= idx)
 			{
 				if (i == 0)
 				{
-					new_node->next = cursor;
-					cursor = new_node;
-					break;
+					new_node->next = *head;
+					*head = new_node;
 				}
-				new_node->next = previous_node->next;
-				previous_node->next = new_node;
-				break;
+				else
+				{
+					new_node->next = previous_node->next;
+					previous_node->next = new_node;
+				}
+				return (new_node);
 			}
-			else if ((i + 1) == idx)
+			else
 			{
-				previous_node = cursor;
+				free(new_node);
+				return (NULL);
 			}
-			cursor = cursor->next;
-			i++;
-		}
-		else
-		{
-			free(new_node);
-			return (NULL);
 		}
 	}
-	return (new_node);
 }
