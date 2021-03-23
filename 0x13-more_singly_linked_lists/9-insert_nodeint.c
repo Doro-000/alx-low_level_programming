b5455da3d771a32c807aa8bdc49ee64aa8c965c1
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-23 13:38:30 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		if (i <= idx)
 		{
