85acd5bc60ef859a4a654df052366434bb6ec190
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-23 15:00:53 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL || idx > listint_len(*head))
 	{
-		free (new_node);
+		free(new_node);
 		return (NULL);
 	}
 	new_node->n = n;
