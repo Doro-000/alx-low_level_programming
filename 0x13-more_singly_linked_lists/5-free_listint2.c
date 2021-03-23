e2fbd45e4a83a727a23c24d5d6846a45d291ae44
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-23 12:36:03 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,12 +11,14 @@ void free_listint2(listint_t **head)
 	listint_t *cursor;
 	listint_t **temp = head;
 
-	while (*head != NULL && temp != NULL)
-	{
-		cursor = *head;
-		*head = (*head)->next;
-		free(cursor);
-	}
 	if (temp != NULL)
+	{
+		while (*head != NULL)
+		{
+			cursor = *head;
+			free(cursor);
+			*head = (*head)->next;
+		}
 		*temp = NULL;
+	}
 }
