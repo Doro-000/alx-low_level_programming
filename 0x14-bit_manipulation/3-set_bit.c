35f8f8d4b9582c9734b62419ed126108e85dcdc3
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-25 13:47:40 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL || (((1 << index) > *n) && *n))
+	if (n == NULL || (((1 << index) > *n) && *n > 0))
 		return (-1);
 	*n |= (1 << index);
 	return (1);
