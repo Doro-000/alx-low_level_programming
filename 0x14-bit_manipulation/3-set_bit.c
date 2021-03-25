1896c8e04fa4ec3082088e860277cd230ce132bb
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-25 13:42:14 +0300

    Update 3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted value
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (((1 << index) > *n) && *n))
+		return (-1);
+	*n |= (1 << index);
+	return (1);
+}
