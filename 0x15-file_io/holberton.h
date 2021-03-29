a0f80ebd1e1c9107c2ae92e5ca6c2e6d8a868792
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-28 23:04:51 -0700

    Update holberton.h

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,19 +2,12 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /*HOLBERTON_H*/
