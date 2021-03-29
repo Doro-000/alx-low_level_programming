9b6ba37f93a1a143a65ad77216e09f8e0776f657
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-28 23:55:55 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,10 +19,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	file = open(filename, O_RDONLY);
-	if (file < 0)
+	if (file == -1)
 		return (0);
 	n_read = read(file, buffer, letters);
-	if (n_read < 0)
+	if (n_read == -1)
 		return (0);
 	buffer[n_read] = '\0';
 	wrote = write(STDOUT_FILENO, buffer, n_read);
