7816b519c60f5d45d0fddb1834f115156a921a91
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-29 21:28:25 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
+int check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
  * main - copies the content of one file to another
  * @argc: argument count
