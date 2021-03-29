64a70d4af28ddcef6c1d6c2b8678b728db347e1e
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-03-28 23:04:49 -0700

    Update 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters:  number of letters to be printed
+ *
+ * Return: nmber of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+
+	if (filename == NULL)
+		return (0);
+}
