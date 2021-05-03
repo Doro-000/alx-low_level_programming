2703d87981f72a709c290f8351f46917742979f7
Doro-000 <hizkyastekle@gmail.com>
2021-05-03 09:04:59 +0000

    solution for advanced task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,25 @@
+/*#include "operations.h"*/
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
