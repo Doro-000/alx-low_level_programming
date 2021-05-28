8a25d88cb7748cadcb533cfcc28a9e2e90d6973d
Doro-000 <hizkyastekle@gmail.com>
2021-05-28 16:08:46 +0000

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,9 +11,9 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL, cursor = NULL;
+	hash_node_t *new_node = NULL, *cursor = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL|| strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
 		return (0);
 	if (ht->size == 0)
 		return (0);
