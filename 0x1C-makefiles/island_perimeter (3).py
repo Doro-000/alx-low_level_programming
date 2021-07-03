44fa10a63e7e933ab8ca4b13109477ce799c6255
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-07-03 17:57:26 +0300

    Update island_perimeter (3).py

--- 0x1C-makefiles/island_perimeter (3).py
+++ 0x1C-makefiles/island_perimeter (3).py
@@ -1,75 +1,92 @@
+#!/usr/bin/python3
+
+
 def island_perimeter(grid):
-    for i in grid:
-        for x in i:
-            if x == 1:
-
-
-def total(grid):
+    """Island perimeter counter
+    Args:
+        grid (matrix): grid representing island
+    Returns:
+        int: perimeter of island
+    """
     sum = 0
-
     for i in range(len(grid)):
-        if i == 0:
-            if grid[i] == 1 and grid[i + 1] != 1:
-                sum += 3 * 1
-            elif grid[i] == 1 and grid[i + 1] == 1:
-                sum += 2 * 1
-            elif grid[i] != 1 and grid[i + 1] == 1:
-                sum += 1
-        elif (i != len(grid) - 1):
-            if grid[i] == 1 and grid[i + 1] == 1:
-                sum += grid[i]
-            elif grid[i] == 1 and grid[i + 1] != 1:
-                sum += 2 * 1
-            elif grid[i] != 1 and grid[i + 1] == 1:
-                sum += 1
-        else:
-            if grid[i] == 1 and grid[i - 1] != 1:
-                sum += 3 * 1
-            elif grid[i] == 1 and grid[i - 1] == 1:
-                sum += 2 * 1
+        for x in range(len(grid[i])):
+            if grid[i][x] == 1 and (i < (len(grid) - 1) and i > 0):
+                fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1)
+                fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
+                fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == (len(grid) - 1)):
+                fb_ver2 = (grid[i - 1][x] != 1 or True)
+                fb_ver_n = (grid[i - 1][x] != 1 and True)
+                fb_ver = (grid[i - 1][x] == 1 and False)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+            elif grid[i][x] == 1 and (i == 0):
+                fb_ver2 = (True or grid[i + 1][x] != 1)
+                fb_ver_n = (True and grid[i + 1][x] != 1)
+                fb_ver = (False and grid[i + 1][x] == 1)
+                sum += total(grid, i, x, fb_ver, fb_ver2, fb_ver_n)
+
     return sum
 
 
-def pass_if_zero(grid):
+def total(grid, i, x, fb_ver, fb_ver2, fb_ver_n):
+    """ sum of a cells perimeter
+    Args:
+        grid (matrix): grid representing island
+        i (int): column
+        x (int): row
+        fb_ver (bool): same column value
+        fb_ver2 (bool): diff col value
+        fb_ver_n (bool): 0 in both cells
+    Returns:
+        int : perimeter of cell
+    """
     sum = 0
-    for i in grid:
-        sum += i
-    if sum == 0:
-        return False
-    else:
-        return True
-
-
-def rows(grid):
-    row = 0
-    for x in grid:
-        sum = 0
-        for i in x:
-            sum += i
-        if sum != 0:
-            row += 1
-    return row
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
-
-    #     fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
-    #     fb_hor = (grid[i][x + 1] == 1 and grid[i][x - 1] == 1)
-    #     fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
-    #     fb_hor_n = (grid[i][x + 1] != 1 and grid[i][x - 1] != 1)
-    #     fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1)
-    #     fb_hor2 = (grid[i][x + 1] != 1 or grid[i][x - 1] != 1)
-    # if x == 0 and grid[i][x] == 1:
-    #     fb_hor = (grid[i][x + 1] == 1)
-    #     fb_hor2 = (grid[i][x + 1] != 1)
-    #     fb_ver = (grid[i + 1][x] == 1 and grid[i - 1][x] == 1)
-    #     fb_ver_n = (grid[i + 1][x] != 1 and grid[i - 1][x] != 1)
-    #     fb_ver2 = (grid[i + 1][x] != 1 or grid[i - 1][x] != 1) and (not fb_ver_n)
+    if x == 0 and grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x + 1] != 1:
+            sum += 3
+        elif fb_ver_n and grid[i][x + 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x + 1] == 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x + 1] == 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x + 1] == 1:
+            sum += 1
+    elif (x != len(grid[i]) - 1) and grid[i][x] == 1:
+        fb_hor = (grid[i][x + 1] == 1 and grid[i][x - 1] == 1)
+        fb_hor_n = (grid[i][x + 1] != 1 and grid[i][x - 1] != 1)
+        fb_hor2 = (grid[i][x + 1] != 1 or grid[i][x - 1] != 1)
+        if (fb_hor2 and (not fb_hor_n)) and (fb_ver2 and (not fb_ver_n)):
+            sum += 2
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver_n:
+            sum += 3
+        elif (fb_hor2 and (not fb_hor_n)) and fb_ver:
+            sum += 1
+        elif fb_hor and fb_ver_n:
+            sum += 2
+        elif fb_hor and (fb_ver2 and (not fb_ver_n)):
+            sum += 1
+        elif fb_hor_n and fb_ver_n:
+            sum += 4
+        elif fb_hor_n and fb_ver:
+            sum += 2
+        elif fb_hor_n and (fb_ver2 and (not fb_ver_n)):
+            sum += 3
+    elif grid[i][x] == 1:
+        if fb_ver2 and (not fb_ver_n) and grid[i][x - 1] != 1:
+            sum += 3
+        elif fb_ver2 and (not fb_ver_n) and grid[i][x - 1] == 1:
+            sum += 2
+        elif fb_ver_n and grid[i][x - 1] != 1:
+            sum += 4
+        elif fb_ver_n and grid[i][x - 1] == 1:
+            sum += 3
+        elif fb_ver and grid[i][x - 1] != 1:
+            sum += 2
+        elif fb_ver and grid[i][x - 1] == 1:
+            sum += 1
+    return sum
