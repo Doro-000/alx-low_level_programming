ada6ad798052d71c2b32b4dec1e7855b3c6971c4
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-31 15:25:13 +0300

    advanced binary search and exponential search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - improvement on binary search algo CAN GET WAY BETTER
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value being searched
+ *
+ * Return: index where value is located, or -1 on failure
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t i;
+	int mid;
+
+	if (array == NULL)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		if (i != size - 1)
+			printf("%d, ", array[i]);
+		else
+			printf("%d\n", array[i]);
+	}
+
+	if (size == 1)
+	{
+		if (*array == value)
+			return (0);
+		return (-1);
+	}
+
+	if (size == 2)
+	{
+		if (array[0] == value)
+			return (0);
+		if (array[1] == value)
+			return (1);
+	}
+
+	if (size % 2 == 0)
+		mid = (size / 2) - 1;
+	else
+		mid = size / 2;
+
+	if (array[mid] > value)
+		return (advanced_binary(array, size / 2, value));
+	else if (array[mid] < value)
+	{
+		int temp = advanced_binary(array + mid + 1, size / 2, value);
+		if (temp != -1)
+			return (temp + (mid + 1));
+		else
+			return (-1);
+	}
+	else
+		return (advanced_binary(array, size / 2 + 1, value));
+}
