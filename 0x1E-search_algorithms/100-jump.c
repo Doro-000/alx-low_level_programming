10632734f45a96a303e59c37cbcddfb0ad19ce5e
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-09-01 12:06:53 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,7 +18,7 @@ int jump_search(int *array, size_t size, int value)
 		return (-1);
 	if (size == 1)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i])
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[0] == value)
 			return (0);
 		return (-1);
