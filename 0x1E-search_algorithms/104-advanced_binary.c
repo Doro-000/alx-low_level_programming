cf9f5457db4a04ae0e923e08abe13861f31a2949
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-09-02 13:37:16 +0300

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -33,23 +33,12 @@ int advanced_binary(int *array, size_t size, int value)
 			return (1);
 	}
 	mid = size % 2 == 0 ? (size / 2) - 1 : size / 2;
-	if (array[mid] > value)
+	if (array[mid] >= value)
 		return (advanced_binary(array, size / 2, value));
 	if (array[mid] < value)
 	{
 		temp = advanced_binary(array + mid + 1, size / 2, value);
-		if (temp != -1)
-			return (temp + (mid + 1));
-		else
-			return (-1);
+		return temp != -1 ? temp + (mid + 1) : -1;
 	}
-	if (array[mid] == value)
-	{
-		temp = advanced_binary(array + mid, size / 2, value);
-		if (temp != -1)
-			return (temp + mid);
-		else
-			return (-1);
-	}		
 	return (-1);
 }
